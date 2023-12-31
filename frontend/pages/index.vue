916b5521b4520907447d05a3fa857a6192de393f
Jeferson Inácio <jeferson.i.silva@gmail.com>
2023-12-31 09:43:47 -0300

    Default page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main style="background:#7f7f7f33;">
+      <v-container>
+        <v-card>
+          <v-card-text>
+            <div>Hello World</div>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
